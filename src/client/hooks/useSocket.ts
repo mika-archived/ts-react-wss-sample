0520a4009e3f4a65291ea461025af0e0fee209dd
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-01 02:17:03 +0900

    feat: unsubscribe old listeners when listener keys changed

--- src/client/hooks/useSocket.ts
+++ src/client/hooks/useSocket.ts
@@ -22,21 +22,24 @@ const useSocket = (
   }, [listeners]);
 
   useEffect(() => {
-    socket.on("connect", () => console.log("connected"));
-    socket.on("disconnect", () => console.log("disconnected"));
+    const previous = refs.current;
+    refs.current = listeners;
 
     const runListener = (event: string, ...args: any[]) => {
       const listener = refs.current[event];
       if (listener) listener(...args);
     };
 
+    Object.keys(previous).forEach(event => socket.removeListener(event));
     Object.keys(listeners).forEach(event => socket.on(event, (...args: any[]) => runListener(event, ...args)));
+  }, [Object.keys(listeners).join("-")]); // when listen events is changed, unsubscribe and subscribe
 
+  useEffect(() => {
     return () => {
       socket.disconnect();
       socket.removeAllListeners();
     };
-  }, [uri, opts]);
+  }, [socket]);
 
   return { socket };
 };
